3056f2f30025f5833caa5b5091b8ae280cd50252
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-08-07 16:46:26 +0530

    preview and meta added!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import BgMusic from "@/components/BgMusic";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import BgMusic from "@/components/BgMusic";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -17,6 +17,31 @@ export const metadata: Metadata = {
     title: "HBench | Human Benchmark",
     description:
         "Train your brain. Test your reflexes. A fun and minimal suite of cognitive challenges, from memory to motor skills.",
+    openGraph: {
+        title: "HBench | Human Benchmark",
+        description:
+            "Train your brain. Test your reflexes. A fun and minimal suite of cognitive challenges, from memory to motor skills.",
+        url: "https://hbench.vercel.app/",
+        siteName: "HBench",
+        images: [
+            {
+                url: "https://hbench.vercel.app/preview.png",
+                width: 1200,
+                height: 630,
+                alt: "HBench - Human Benchmark Preview",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "HBench | Human Benchmark",
+        description:
+            "Train your brain. Test your reflexes. A fun and minimal suite of cognitive challenges, from memory to motor skills.",
+        images: ["https://hbench.vercel.app/preview.png"],
+        creator: "@prabhatsuntoh",
+    },
 };
 
 export default function RootLayout({
